367b7bb07f660fe6d58d46536b8baa06618b5ad1
xia0deviZ <lamjuyan@gmail.com>
2025-02-24 14:36:52 +0800

    assignment3

---
+++ script.js
@@ -0,0 +1,42 @@
+const msftData = [190.15, 194.21, 191.07, 190.44, 200.15]
+
+let total =0;
+total += msftData[0]
+total += msftData[1]
+total += msftData[2]
+total += msftData[3]
+total += msftData[4]
+
+console.log (total /5)
+
+const msftaData = [190.15, 194.21, 191.07, 190.44, 200.15]
+
+msftData.push("205.67")
+
+console.log(msftData)
+
+
+const disneyData = {
+	open: "120.54",
+	high: "122.56",
+	low: "120.54",
+	close: "121.09"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
